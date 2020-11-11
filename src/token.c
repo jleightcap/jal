0df708c4ec1c6e64afce01529977c3f567ea61b8
jleightcap <jleightcap@gmail.com>
2020-11-11 17:28:42 -0500

    split out scan() to subroutines (scan_string, scan_symbol)
    starting to need to scan() multiple files because of imports.
    have 1 'global' scan that maintains a pointer into the main source file.
    but, need some of those pieces to scan from imported files for signatures.

--- src/token.c
+++ src/token.c
@@ -9,6 +9,7 @@ static int fp = 0;              // file position
 static const char* file = NULL; // global FILE for tokenizing
 static int len = 0;             // global FILE length
 
+// initialize global variables
 void
 setstream(const char stream[], int streamlen)
 {
@@ -16,8 +17,51 @@ setstream(const char stream[], int streamlen)
     len = streamlen;
 }
 
+void
+scan_symbol(struct token* tok, const char* stream, int* fp)
+{
+    // to reduce the number of hash table collisions, keep track of the
+    // length of symbol tokens. If some token happens to hash to same hash as a
+    // reserved keyword, lengths must also match for a collision to occur.
+    unsigned int symlen = 0;
+    unsigned long hash = file[*fp];
+    // symbols can't start with number characters, but can include them
+    for(; issymbol(file[*fp]) || isnum(file[*fp]); (*fp)++) {
+        // TODO: play with prime numbers in hashing
+        hash = (hash * 67 + file[*fp]) % ENV_SIZE;
+        symlen++;
+    }
+    tok->value.hash = hash;
+
+    // RESERVED KEYWORD HASHES
+    if(hash == hashstr("main") && symlen == 4) {
+        tok->type = MAIN;
+    }
+    else if(hash == hashstr("import") && symlen == 6) {
+        tok->type = IMPORT;
+    }
+    else if(hash == hashstr("defun") && symlen == 5) {
+        tok->type = DEFUN;
+    }
+    else if(hash == hashstr("devar") && symlen == 5) {
+        tok->type = DEVAR;
+    }
+    else if(hash == hashstr("ret") && symlen == 3) {
+        tok->type = RET;
+    }
+    else if(hash == hashstr("while") && symlen == 5) {
+        tok->type = WHILE;
+    }
+    else if(hash == hashstr("for") && symlen == 3) {
+        tok->type = FOR;
+    }
+    else {
+        tok->type = SYM;
+    }
+}
+
 struct token
-scan()
+scan(void)
 {
     assert(file != NULL && "file stream not initialized: use setstream!");
     assert(len  != 0    && "len not initialized: use setstream!");
@@ -37,41 +81,7 @@ scan()
 
     // SYMBOLS
     if(issymbol(file[fp])) {
-        // to reduce the number of hash table collisions, keep track of the
-        // length of symbol tokens. If some variable name happens to hash
-        // to same hash as 'while', lengths must also match for a collision.
-        unsigned int symlen = 0;
-        unsigned long hash = file[fp];
-        // symbols can't start with number characters, but can include them
-        for(; issymbol(file[fp]) || isnum(file[fp]); fp++) {
-            // TODO: play with prime numbers in hashing
-            hash = (hash * 67 + file[fp]) % ENV_SIZE;
-            symlen++;
-        }
-        tok.value.hash = hash;
-
-        // RESERVED KEYWORD HASHES
-        if(hash == hashstr("main") && symlen == 4) {
-            tok.type = MAIN;
-        }
-        else if(hash == hashstr("defun") && symlen == 5) {
-            tok.type = DEFUN;
-        }
-        else if(hash == hashstr("devar") && symlen == 5) {
-            tok.type = DEVAR;
-        }
-        else if(hash == hashstr("ret") && symlen == 3) {
-            tok.type = RET;
-        }
-        else if(hash == hashstr("while") && symlen == 5) {
-            tok.type = WHILE;
-        }
-        else if(hash == hashstr("for") && symlen == 3) {
-            tok.type = FOR;
-        }
-        else {
-            tok.type = SYM;
-        }
+        scan_symbol(&tok, file, &fp);
         return tok;
     }
 
@@ -180,4 +190,37 @@ scan()
     // No matches! Tokenizing failed.
     fprintf(stderr, "token parsing error!\n");
     exit(-1);
+}
+
+// return the next token's literal string value
+// callee must free returned char*
+char*
+scan_string(void)
+{
+    assert(file != NULL && "file stream not initialized: use setstream!");
+    assert(len  != 0    && "len not initialized: use setstream!");
+
+    char* str = malloc(MAX_STRLEN * sizeof(char));
+
+    // skip whitespace and comments ('#' to '\n' or EOF)
+    while(iswhitespace(file[fp])) fp++;
+    while(file[fp] == '#') {
+        while(file[fp] != '\n' && file[fp] != EOF) fp++;
+        while(iswhitespace(file[fp])) fp++;
+    }
+    if(fp >= len) { panic("scanned past end of file when expecting a string!"); }
+
+    assert(file[fp++] == '"' && "expected start of string in scan_string!");
+    unsigned long ii = 0; // long probably overkill, but in case MAX_STRLEN > 256
+    while(file[fp] != '"') {
+        str[ii] = file[fp];
+        fp++; ii++;
+        assert(ii < MAX_STRLEN - 1 && "exceeded string buffer in scan_string, increase MAX_STRLEN!");
+    }
+    assert(file[fp] == '"' && "expected end of string in scan_string!");
+
+    str[ii + 1] = '\0'; // null terminate string
+    fp++;
+
+    return str;
 }
